95f83af68dd1e351fb4ff7e62d5eeb98c8fb8a0b
2our7av <27stylesprod@gmail.com>
2026-03-06 13:55:00 +0300

    подтягивает с бд успеваемость

--- BaumanCore/BaumanCore/Features/GradesView/Grades_main_structure.swift
+++ BaumanCore/BaumanCore/Features/GradesView/Grades_main_structure.swift
@@ -1,4 +1,6 @@
 import SwiftUI
+import Combine
+import FirebaseAuth
 
 struct Grades: View {
     @State private var selectedTab: Tab = .current
@@ -11,6 +13,8 @@ struct Grades: View {
     @State private var subjects: [SubjectData] = []
     @State private var semesters: [Semester] = []
 
+    @StateObject private var studentVM = StudentViewModel()
+    
     var body: some View {
         ZStack(alignment: .top) {
             ScrollView {
@@ -33,20 +37,19 @@ struct Grades: View {
                 .zIndex(1)
         }
         .onAppear {
-            FirebaseService().fetchStudent { student in
-                if let student = student {
-                    self.subjects = student.subjects
-                    self.semesters = student.semesters
-                } else {
-                    self.subjects = []
-                    self.semesters = []
-                }
-            }
+            studentVM.fetchStudent()
+        }
+        
+        
+        // Подписываемся на обновления ViewModel
+        .onReceive(studentVM.$subjects) { subjects in
+            self.subjects = subjects
+        }
+        .onReceive(studentVM.$semesters) { semesters in
+            self.semesters = semesters
         }
     }
 }
-
-
 
 struct SessionTabView: View {
     var semesters: [Semester]
